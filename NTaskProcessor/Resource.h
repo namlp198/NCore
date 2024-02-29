3e60ffbbca9d3689a226173cb6e8a3e05cf9879e
nlp <namlp1908@gmail.com>
2024-02-29 18:25:39 +0700

    [02/29/2024] - add new LogView and Resource.h

---
+++ NTaskProcessor/Resource.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define IDD_DIALOG_LOG                  4000
+#define IDC_LIST_LOG                    4000
+#define IDC_CHECK_ALWAYS_TOP_ON         4001
+#define IDC_STATIC_LOG_PATH             4002
+#define IDC_BUTTON_OPEN_PATH            4003
