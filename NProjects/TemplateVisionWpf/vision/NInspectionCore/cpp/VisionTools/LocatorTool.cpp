dd06f50ead7ddb6897a3beff82bb78d2fa237273
nampc1618 <nampc1618@gmail.com>
2024-03-23 01:20:48 +0700

    [03-23-2024] - update vision NInspectionCore

--- NProjects/TemplateVisionWpf/vision/NInspectionCore/cpp/VisionTools/LocatorTool.cpp
+++ NProjects/TemplateVisionWpf/vision/NInspectionCore/cpp/VisionTools/LocatorTool.cpp
@@ -9,15 +9,36 @@ CLocatorTool::~CLocatorTool()
 {
 }
 
-void CLocatorTool::Run()
+LPBYTE CLocatorTool::GetTemplateImageBuffer()
+{
+	if (m_pImageTemplate->empty())
+		return nullptr;
+
+	return (LPBYTE)m_pImageTemplate->data;
+}
+
+BOOL CLocatorTool::GetDataTrained(DataTrained* pDataTrained)
+{
+	return 0;
+}
+
+BOOL CLocatorTool::Run()
 {
 	NVision_FindLocator();
 }
 
-void CLocatorTool::NVision_FindLocator()
+BOOL CLocatorTool::SaveImageTemplate(CString saveImagePath)
+{
+}
+
+BOOL CLocatorTool::NVision_FindLocator()
 {
 	//AfxMessageBox(_T("This is tool: " + m_ParamLoca.m_csName));
 
 	// implement algorithm at here
 
+}
+
+BOOL CLocatorTool::NVision_TrainLocator(RectForTrainLocTool* paramTrainLoc)
+{
 }
--- NProjects/TemplateVisionWpf/vision/NInspectionCore/header/VisionTools/LocatorTool.h
+++ NProjects/TemplateVisionWpf/vision/NInspectionCore/header/VisionTools/LocatorTool.h
@@ -5,6 +5,24 @@
 #include "VisionManager.h"
 #include "interface_vision.h"
 
+struct DataTrained
+{
+	int m_nCntX;
+	int m_nCntY;
+};
+
+struct RectForTrainLocTool
+{
+	int m_nRectIn_X;
+	int m_nRectIn_Y;
+	int m_nRectIn_Width;
+	int m_nRectIn_Height;
+	int m_nRectOut_X;
+	int m_nRectOut_Y;
+	int m_nRectOut_Width;
+	int m_nRectOut_Height;
+};
+
 class AFX_EXT_CLASS CLocatorTool
 {
 public:
@@ -16,21 +34,31 @@ public:
 	CLocatorToolResult     GetLocaToolRes() { return m_LocaResult; }
 	CParameterLocator      GetParamLoca() { return m_ParamLoca; }
 	LPBYTE                 GetImageBuffer() { return m_pImageBuffer; }
+	LPBYTE                 GetTemplateImageBuffer();
+	BOOL                   GetDataTrained(DataTrained* pDataTrained);
 
 	// setter
 	void                   SetLocaToolRes(CLocatorToolResult locaToolRes) { m_LocaResult = locaToolRes; }
 	void                   SetParamLoca(CParameterLocator paramLoca) { m_ParamLoca = paramLoca; }
 	void                   SetImageBuffer(LPBYTE pImgBuff) { m_pImageBuffer = pImgBuff; }
 public:
-	void                    Run();
+	BOOL                   Run();
+	BOOL                   SaveImageTemplate(CString saveImagePath);
 
 protected:
-	void                    NVision_FindLocator();
+	BOOL                    NVision_FindLocator();
+	BOOL                    NVision_TrainLocator(RectForTrainLocTool* paramTrainLoc);
 
 private:
+	CString                         m_templateImagePath;
+
 	CParameterLocator               m_ParamLoca;
 	CLocatorToolResult              m_LocaResult;
 
+	RectForTrainLocTool             m_paramTrainLoc;
+	DataTrained                     m_dataTrained;
+
 	LPBYTE                          m_pImageBuffer;
-	cv::Mat*                        m_ResultImageBuffer;
+	cv::Mat*                        m_pResultImageBuffer;
+	cv::Mat*                        m_pImageTemplate;
 };
