dc5a2ab69f451d40a9728e422ce08d6e6e67438f
namlp198 <namlp1908@gmail.com>
2024-05-14 18:37:06 +0700

    [05-14-2024] - init project

---
+++ Test_SerialPort/serialport/serial_port_info.h
@@ -0,0 +1,66 @@
+#pragma once
+#ifndef SERIAL_PORT_INFO_H
+#define SERIAL_PORT_INFO_H
+
+//************Content************
+
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <atlbase.h>
+
+namespace SerialPortUtils
+{
+    /**
+     * @brief Serial Port Information
+     */
+    class SerialPortInfo
+    {
+        public:
+            static std::vector<std::string> toFriendlyNameList(std::vector<SerialPortInfo> serialPorts);
+            static std::vector<int> toPortList(std::vector<SerialPortInfo> serialPorts);
+
+        public:
+            /**
+             * @brief Port. Default as -1.
+             */
+            int port = -1;
+
+            /**
+             * @brief Device Name use for QueryDosDevice()
+             */
+            LPTSTR queryDosDeviceName = (LPTSTR)L"";
+            
+            /**
+             * @brief Device Names
+             */
+            std::string deviceName = "";
+
+            /**
+             * @brief Friendly Name
+             */
+            std::string friendlyName = "";
+
+            // To string
+
+            /**
+             * @brief To String
+             * 
+             * @return Return string
+             */
+            operator std::string() const
+            {
+                return friendlyName;
+            }
+
+            friend std::ostream& operator << (std::ostream& out, const SerialPortInfo& obj) {
+                return out << (std::string)obj;
+            }
+    };
+}
+
+
+//*******************************
+
+#endif
