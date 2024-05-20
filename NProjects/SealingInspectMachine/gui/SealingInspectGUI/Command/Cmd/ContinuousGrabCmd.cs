d5c0249ad4600e58acc4e686d8a7ce8fc3311735
namlp198 <namlp1908@gmail.com>
2024-05-20 20:46:23 +0700

    [05-20-2024] - add new command

---
+++ NProjects/SealingInspectMachine/gui/SealingInspectGUI/Command/Cmd/ContinuousGrabCmd.cs
@@ -0,0 +1,32 @@
+﻿using SealingInspectGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SealingInspectGUI.Command.Cmd
+{
+    public class ContinuousGrabCmd : CommandBase
+    {
+        public ContinuousGrabCmd() { }
+        public override void Execute(object parameter)
+        {
+            if (!MainViewModel.Instance.SettingVM.IsStreamming)
+            {
+                MainViewModel.Instance.SettingVM.SettingView.Dispatcher.BeginInvoke(new Action(async () =>
+                {
+                    await MainViewModel.Instance.SettingVM.m_cameraStreamingController.ContinuousGrab(Manager.Class.CameraType.Hik);
+                }));
+            }
+            else
+            {
+                MainViewModel.Instance.SettingVM.SettingView.Dispatcher.BeginInvoke(new Action(async () =>
+                {
+                    await MainViewModel.Instance.SettingVM.m_cameraStreamingController.StopGrab(Manager.Class.CameraType.Hik);
+                }));
+            }
+        }
+    }
+}
---
+++ NProjects/SealingInspectMachine/gui/SealingInspectGUI/Command/Cmd/SingleGrabCmd.cs
@@ -0,0 +1,22 @@
+﻿using SealingInspectGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SealingInspectGUI.Command.Cmd
+{
+    public class SingleGrabCmd : CommandBase
+    {
+        public SingleGrabCmd() { }
+        public override void Execute(object parameter)
+        {
+            MainViewModel.Instance.SettingVM.SettingView.Dispatcher.BeginInvoke(new Action(() =>
+            {
+               MainViewModel.Instance.SettingVM.m_cameraStreamingController.SingleGrab();
+            }));
+        }
+    }
+}
