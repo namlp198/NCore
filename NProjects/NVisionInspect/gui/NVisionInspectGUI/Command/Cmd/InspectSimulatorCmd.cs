cd2eeedd69c92f3b406a64eba11da9c31fc1b181
nampc1618 <nampc1618@gmail.com>
2024-11-18 22:24:57 +0700

    [2024-11-18] - update gui NVisionInspect

---
+++ NProjects/NVisionInspect/gui/NVisionInspectGUI/Command/Cmd/InspectSimulatorCmd.cs
@@ -0,0 +1,21 @@
+﻿using NVisionInspectGUI.Commons;
+using NVisionInspectGUI.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NVisionInspectGUI.ViewModels;
+
+namespace NVisionInspectGUI.Command.Cmd
+{
+    public class InspectSimulatorCmd : CommandBase
+    {
+        public InspectSimulatorCmd() { }
+        public override void Execute(object parameter)
+        {
+            int nCamIdx = MainViewModel.Instance.SettingVM.SettingView.buffSettingPRO.CameraIndex;
+            InterfaceManager.Instance.m_processorManager.m_NVisionInspectProcessorDll.Inspect_Simulator((int)emCameraBrand.CameraBrand_Hik, nCamIdx);
+        }
+    }
+}
